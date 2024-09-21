9c25c752dd9f0aaca1da0a2c6c0c2e07a7eb9f5b
Huysor kheang <lokmjassor@gmail.com>
2024-09-22 01:51:18 +0700

    custom global exception

--- src/exception/custom-exception-response.exception.ts
+++ src/exception/custom-exception-response.exception.ts
@@ -1,5 +1,6 @@
 import {
   ArgumentsHost,
+  BadRequestException,
   Catch,
   ExceptionFilter,
   HttpException,
@@ -7,11 +8,23 @@ import {
 import { Response } from 'express';
 import { ResultRep } from 'src/common/dto/result.dto';
 
-@Catch(HttpException)
-export class CustomResponse implements ExceptionFilter {
+@Catch(ExternalError)
+export class ExternalError implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const res = context.getResponse<Response>();
-    res.send(ResultRep.failedWithMessage(exception.message));
+    const msg = exception.getResponse() as any;
+    res.send(ResultRep.failedWithMessage(msg.message));
+  }
+}
+@Catch(BadRequestException)
+export class ValidationExceptionFilter implements ExceptionFilter {
+  catch(exception: BadRequestException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    response
+      .status(status)
+      .json(ResultRep.failedWithMessage(exception.message));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  ExternalError,
+  ValidationExceptionFilter,
+} from './exception/custom-exception-response.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const messages = errors
+          .map((error) => Object.values(error.constraints))
+          .flat()
+          .join(', ');
+        return new BadRequestException(messages || 'Validation failed');
+      },
+    }),
+  );
+  app.useGlobalFilters(new ValidationExceptionFilter(), new ExternalError());
   await app.listen(4001);
 }
 bootstrap();
